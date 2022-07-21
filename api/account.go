af777f1fdbe3c17e15577acd7a6d2bf2e0b78eab
Cristian Webber <cristian.webber@hotmail.com>
2022-07-21 20:29:30 -0300

    style: fix bindings

--- api/account.go
+++ api/account.go
@@ -37,7 +37,7 @@ func (server *Server) createAccount(ctx *gin.Context) {
 }
 
 type getAccountRequest struct {
-	ID int64 `uri:"id" binding:required,min=1`
+	ID int64 `uri:"id" binding:"required,min=1"`
 }
 
 func (server *Server) getAccount(ctx *gin.Context) {
@@ -61,8 +61,8 @@ func (server *Server) getAccount(ctx *gin.Context) {
 }
 
 type listAccountRequest struct {
-	PageID int32 `form:"page_id" binding:required,min=1`
-	PageSize int32 `form:"page_size" binding:required,min=5,max=10`
+	PageID int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
 func (server *Server) listAccount(ctx *gin.Context) {
